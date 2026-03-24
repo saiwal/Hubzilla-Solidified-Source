21a623cdd33f06580f69cead3a91a30bb3145cff
SK <sk@utsukta.org>
2026-03-24 16:20:57 +0530

    filters

--- src/modules/network/routes.tsx
+++ src/modules/network/routes.tsx
@@ -1,8 +1,7 @@
 import { onMount, onCleanup, Show, For } from "solid-js";
-import { useSearchParams } from "@solidjs/router";
 import { posts, loadNetwork, loading, loadMore, loadingMore, hasMore, newPosts, flushNewPosts } from "./store";
 import StreamList from "../../components/StreamList";
-import type { NetworkParams } from "./api";
+import StreamFilters from "./StreamFilters";
 
 function PostPlaceholder() {
   return (
@@ -34,48 +33,24 @@ function PostPlaceholder() {
 }
 
 export default function Network() {
-  const [searchParams] = useSearchParams();
   let sentinelRef!: HTMLDivElement;
 
   onMount(() => {
-    const s = (key: string) => {
-      const v = searchParams[key];
-      return v ? String(Array.isArray(v) ? v[0] : v) : undefined;
-    };
+      // StreamFilters calls loadNetwork on mount via its own apply(),
+    // but we still need an initial load with defaults if filters haven't fired yet
+    loadNetwork({ order: "created" });
 
-    const params: NetworkParams = { order: 'created' };
-    const order = s('order');
-    if (order)              params.order  = order as NetworkParams['order'];
-    else params.order = "created";
-    if (s('search'))        params.search = s('search');
-    if (s('tag'))           params.tag    = s('tag');
-    if (s('cat'))           params.cat    = s('cat');
-    if (s('verb'))          params.verb   = s('verb');
-    if (s('xchan'))         params.xchan  = s('xchan');
-    if (s('net'))           params.net    = s('net');
-    if (s('dend'))          params.dend   = s('dend');
-    if (s('dbegin'))        params.dbegin = s('dbegin');
-    if (s('gid'))           params.gid    = Number(s('gid'));
-    if (s('cid'))           params.cid    = Number(s('cid'));
-    if (s('cmin'))          params.cmin   = Number(s('cmin'));
-    if (s('cmax'))          params.cmax   = Number(s('cmax'));
-    if (searchParams.star)  params.star   = 1;
-    if (searchParams.conv)  params.conv   = 1;
-    if (searchParams.dm)    params.dm     = 1;
-
-    loadNetwork(params);
-
-    // Infinite scroll sentinel
     const observer = new IntersectionObserver(
       ([entry]) => { if (entry.isIntersecting) loadMore(); },
-      { rootMargin: '200px' }
+      { rootMargin: "200px" }
     );
     observer.observe(sentinelRef);
-    onCleanup(() => observer.disconnect());
-  });
+    onCleanup(() => observer.disconnect()); 
+	});
 
   return (
     <div class="relative">
+      <StreamFilters />
       {/* New posts banner */}
       <Show when={newPosts().length > 0}>
         <div class="sticky top-2 z-10 flex justify-center">
