b282b976b2de21924985273a02f2dec447879635
SK <sk@utsukta.org>
2026-04-09 15:46:06 +0530

    feed icons

--- src/modules/network/views/ViewSwitcher.tsx
+++ src/modules/network/views/ViewSwitcher.tsx
@@ -1,13 +1,14 @@
 // components/ViewSwitcher.tsx
+import { MdFillAll_inbox, MdFillApps, MdFillFormat_list_bulleted } from 'solid-icons/md';
 import { changeView, viewMode } from '../store/store';
 import type { ViewMode } from '../store/store';
-import { For } from 'solid-js';
-
-const views: { id: ViewMode; label: string }[]  = [
-  { id: 'feed',    label: 'Feed' },
-  { id: 'masonry', label: 'Grid' },
-  { id: 'list',    label: 'List' },
-  { id: 'inbox',   label: 'Inbox'},
+import { For, type Component } from 'solid-js';
+type IconType = Component<{ size?: number; class?: string }>;
+const views: { id: ViewMode; label: string; icon: IconType  }[]  = [
+  { id: 'feed',    label: 'Feed', icon: MdFillFormat_list_bulleted },
+  { id: 'masonry', label: 'Grid', icon: MdFillApps},
+  { id: 'list',    label: 'List', icon: MdFillFormat_list_bulleted},
+  { id: 'inbox',   label: 'Inbox', icon: MdFillAll_inbox},
 ] as const;
 
 export default function ViewSwitcher() {
@@ -20,9 +21,10 @@ export default function ViewSwitcher() {
               ${viewMode() === v.id
                 ? 'bg-gray-200 dark:bg-gray-700 font-medium'
                 : 'hover:bg-gray-100 dark:hover:bg-gray-800 text-gray-500'}`}
+						title={v.label}
             onClick={() => changeView(v.id)}
           >
-            {v.label}
+          <v.icon size={18} />
           </button>
         )}
       </For>
